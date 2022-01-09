7e7296242f432bc14dec4abfcc77f93382af710c
cch0807 <https://github.com/cch0807>
2022-01-09 11:10:36 +0900

    test

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+  name = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'menu'
+
+class Categories(models.Model):
+  name = models.CharField(max_length=45)
+  menu = models.ForeignKey('Menu', on_delete= models.CASCADE)
+
+  class Meta:
+    db_table = 'categories'
+
